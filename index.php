8ad3d1ab03cc83e065dd8495d343f707bc68596c
Obilalkenobill <50761645+Obilalkenobill@users.noreply.github.com>
2020-08-17 13:08:07 +0200

    Add files via upload
    add cursus + style +html

--- index.php
+++ index.php
@@ -137,6 +137,16 @@
                     d’une interface utilisateur qui consommera l’API.</li>
                     </ul>
                 </div>   
+                <div class="dat5">
+                    2019-2021<br>
+                    Gosselies 6041<br>
+                </div>
+                <div class="exp5">
+                    I.F.A.P.M.E.- Developpement Web<br>
+                    <ul>
+                    <li> Application de l'UML à PHP/JAVASCRIPT</li>
+                    </ul>
+                </div>   
             </div>
         </div>
         <div class="lang">
@@ -169,5 +179,42 @@
         </div>
     </div>
     <img class="certif" src="certificat.PNG" alt="certificat de résussite">
-</body>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body>
 </html>
